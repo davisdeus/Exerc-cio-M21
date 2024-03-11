8d6894fca492b7fb169f9fa77c955989f3301c52
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2024-03-11 12:32:32 -0300

    criando novas seções e definindo as fontes

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,30 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+const imagemin = require('gulp-imagemin');
+const uglify = require('gulp-uglify');
 
 function styles() {
     return gulp.src('./src/styles/*.scss')
-    .pipe(sass({outputStyle:'compressed' }))
-    .pipe(gulp.dest('./dist/css'));
+        .pipe(sass({ outputStyle: 'compressed' }))
+        .pipe(gulp.dest('./dist/css'));
 }
 
-exports.default = styles;
-exports.watch = function() {
-    gulp.watch('./src/styles/*.scss', gulp.parallel(styles))
+function images() {
+    return gulp.src('./src/images/**/*')
+        .pipe(imagemin())
+        .pipe(gulp.dest('./dist/images'));
+}
+
+function scripts() {
+    return gulp.src('./src/scripts/*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./dist/scripts'))
+}
+
+
+exports.default = gulp.parallel(styles, images, scripts)
+
+exports.watch = function () {
+    gulp.watch('./src/styles/*.scss', gulp.parallel(styles));
+    gulp.watch('./src/scripts/*scripts', gulp.parallel(scripts));
 }
---
+++ src/scripts/script.js
@@ -0,0 +1,12 @@
+// const btn = document.querySelector('#local_de_gravacao');
+// const curiosityNav = document.querySelector('.curiosity__nav');
+
+// btn.addEventListener('click', function() {
+//     if(curiosityNav.style.display === 'block') {
+//         curiosityNav.style.display = 'none';
+
+//     } else {
+//         curiosityNav.style.display = 'block';
+//     }
+// })
+
