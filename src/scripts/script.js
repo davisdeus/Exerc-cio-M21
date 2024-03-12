d603a23d4eb57a5ef4a7e394bf59e39e01d0b6d9
Davi Silva de Deus <davisilvadedeus18@gmail.com>
2024-03-11 22:57:40 -0300

    configurando as abas locais de gravação, Elenco e História

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,12 +1,34 @@
-// const btn = document.querySelector('#local_de_gravacao');
-// const curiosityNav = document.querySelector('.curiosity__nav');
+document.addEventListener('DOMContentLoaded', function () {
+    const buttons = document.querySelectorAll('[data-nav-button]');
 
-// btn.addEventListener('click', function() {
-//     if(curiosityNav.style.display === 'block') {
-//         curiosityNav.style.display = 'none';
 
-//     } else {
-//         curiosityNav.style.display = 'block';
-//     }
-// })
+    for (let i = 0; i < buttons.length; i++) {
 
+        buttons[i].addEventListener('click', function (botao) {
+            
+            const abaAlvo = botao.target.dataset.navButton;
+            const aba = document.querySelector(`[data-nav-id=${abaAlvo}]`);
+            esconderTodasAsAbas();
+            aba.classList.add('curiosity__list--is-active');
+            removeBotaoAtivo();
+            botao.target.classList.add('curiosity__nav__button--is-active');
+        })
+    }
+})
+
+
+function removeBotaoAtivo() {
+    const buttons = document.querySelectorAll('[data-nav-button]');
+
+    for (let i = 0; i < buttons.length; i++) {
+        buttons[i].classList.remove('curiosity__nav__button--is-active');
+    }
+}
+
+function esconderTodasAsAbas() {
+    const navContainer = document.querySelectorAll('[data-nav-id]');
+
+    for (let i = 0; i < navContainer.length; i++) {
+        navContainer[i].classList.remove('curiosity__list--is-active');
+    }
+}
